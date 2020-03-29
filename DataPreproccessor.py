75bbd4f81705974a23592c0787b8b096ab3d9ebf
Bhavya Kala <bhavya.kala10@gmail.com>
2020-03-29 22:07:20 +0530

    feature extract function for image added

--- DataPreproccessor.py
+++ DataPreproccessor.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
 import csv,string
-class DataPreproccesor:
+import json
+from keras.applications import  VGG19
+from keras.applications.vgg19 import preprocess_input
+from keras.models import Model
+from PIL import Image
+from keras.preprocessing.image import load_img,img_to_array
+from os import listdir
+class Dp:
     address = ''
     desc = ''
     word_count = {}
@@ -11,6 +18,8 @@ class DataPreproccesor:
     word_to_ix = {}
     ix_to_word = {}
     max_len = 0
+    vocab_len = 0 
+    features = {}
     def __init__(self,address):
         super().__init__()
         self.address = address
@@ -36,6 +45,8 @@ class DataPreproccesor:
         for i,ds in mapping.items():
             for d in ds :
                 writer.writerow([i,d])
+        fr.close()
+        fw.close()        
         return mapping    
     def cleaning(self):
         df = pd.read_csv(self.desc,header=0)
@@ -62,6 +73,7 @@ class DataPreproccesor:
         for i in df.index :
             line = df['desc'][i] 
             [self.vocab.update(line.split())]
+        self.vocab_len = len(self.vocab)    
         return self.vocab            
     def word_index(self) :
         i = 1
@@ -78,11 +90,35 @@ class DataPreproccesor:
             l = len(line.split())
             if self.max_len < l :
                 self.max_len = l
-        return self.max_len                
+        return self.max_len  
+    def feature_extract(self,directory) :
+        model = VGG19()
+        model.layers.pop()
+        model = Model(input=model.inputs, outputs=model.layers[-1].output)
+        print(model.summary())
+        for name in listdir(directory) :
+            filename = directory + '/' + name
+            image = load_img(filename, target_size=(224,224))
+            image = img_to_array(image)
+            image = np.expand_dims(image,axis=0)
+            image = preprocess_input(image)
+            feature = model.predict(image)
+            image_id = name.split('.')[0]
+            self.features[image_id] = feature
+        for i in self.features.keys() :
+            self.features[i] = self.features[i].tolist()
+        with open('features.json', 'w') as f:
+            json.dump(self.features,f)  
+        with open('features.json', 'r') as f:
+            self.features = json.load(f)
+        for i in self.features.keys() :
+            self.features[i] = np.array(self.features[i])           
+        return self.features    
 if __name__ == "__main__":
-    ob = DataPreproccesor("D:\Coding_wo_cp\Image_Caption_Generator\Flickr8k.token.txt")
+    ob  = Dp("D:\Coding_wo_cp\Image_Caption_Generator\Flickr8k.token.txt")
     d = ob.to_dictionary()
     df = ob.cleaning()
     vocab = ob.to_vocabulary()
     word_to_ix, ix_to_word = ob.word_index()
     m = ob.maxLength()
+    features = ob.feature_extract('D:\Coding_wo_cp\Image_Caption_Generator\Flicker8k_Dataset')   
