f7e353648b2dd2532a1bb32126150b801fdc1d65
Bhavya Kala <bhavya.kala10@gmail.com>
2020-04-02 17:58:34 +0530

    create_sequences added

--- DataPreproccessor.py
+++ DataPreproccessor.py
@@ -7,8 +7,10 @@ import json
 from keras.applications import  VGG19
 from keras.applications.vgg19 import preprocess_input
 from keras.models import Model
-from PIL import Image
 from keras.preprocessing.image import load_img,img_to_array
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from PIL import Image
 from os import listdir
 class Dp:
     address = ''
@@ -20,9 +22,15 @@ class Dp:
     max_len = 0
     vocab_len = 0 
     features = {}
+    df = pd.DataFrame()
+    photo = {}
+    dataset = []
+    description_dataset = {}
+    X1,X2,y = list(), list(), list()
     def __init__(self,address):
         super().__init__()
         self.address = address
+
     def to_dictionary(self) :
         mapping = dict()
         fr = open(self.address,"r")
@@ -47,26 +55,28 @@ class Dp:
                 writer.writerow([i,d])
         fr.close()
         fw.close()        
-        return mapping    
+        return mapping   
+
     def cleaning(self):
-        df = pd.read_csv(self.desc,header=0)
+        self.df = pd.read_csv(self.desc,header=0)
         # print(df['desc'])
-        df['desc'] = df['desc'].str.lower()
-        df['desc'] = df['desc'].str.translate(str.maketrans('', '', string.punctuation))
-        df['desc'] = df['desc'].str.replace(r'\b\w\b',' ')
+        self.df['desc'] = self.df['desc'].str.lower()
+        self.df['desc'] = self.df['desc'].str.translate(str.maketrans('', '', string.punctuation))
+        self.df['desc'] = self.df['desc'].str.replace(r'\b\w\b',' ')
         d = []
-        for i in df.index:
-            line = df['desc'][i]
+        for i in self.df.index:
+            line = self.df['desc'][i]
             for word in line.split():
                 if word.isalpha():
                     d.append(word)
             if(len(d)>1) :
-                df['desc'][i] = 'startseq ' + " ".join(d) + ' endseq' 
+                self.df['desc'][i] = 'startseq ' + " ".join(d) + ' endseq' 
             else:
-                df.drop(df.index[i])    
+                self.df.drop(self.df.index[i])    
             d.clear()                   
-        df.to_csv('desc.csv')
-        return df                
+        self.df.to_csv('desc.csv')
+        return self.df          
+
     def to_vocabulary(self) :
         df = pd.read_csv(self.desc,header = 0)
         l = []
@@ -75,13 +85,15 @@ class Dp:
             [self.vocab.update(line.split())]
         self.vocab_len = len(self.vocab)    
         return self.vocab            
+
     def word_index(self) :
         i = 1
         for w in self.vocab :
             self.word_to_ix[w] = i
             self.ix_to_word[i] = w
             i+=1
-        return self.word_to_ix,self.ix_to_word      
+        return self.word_to_ix,self.ix_to_word  
+
     def maxLength(self) :
         df = pd.read_csv(self.desc,header = 0)
         l = len(df['desc'][0].split())
@@ -91,29 +103,65 @@ class Dp:
             if self.max_len < l :
                 self.max_len = l
         return self.max_len  
+
     def feature_extract(self,directory) :
-        model = VGG19()
-        model.layers.pop()
-        model = Model(input=model.inputs, outputs=model.layers[-1].output)
-        print(model.summary())
-        for name in listdir(directory) :
-            filename = directory + '/' + name
-            image = load_img(filename, target_size=(224,224))
-            image = img_to_array(image)
-            image = np.expand_dims(image,axis=0)
-            image = preprocess_input(image)
-            feature = model.predict(image)
-            image_id = name.split('.')[0]
-            self.features[image_id] = feature
-        for i in self.features.keys() :
-            self.features[i] = self.features[i].tolist()
-        with open('features.json', 'w') as f:
-            json.dump(self.features,f)  
+        # model = VGG19()
+        # model.layers.pop()
+        # model = Model(input=model.inputs, outputs=model.layers[-1].output)
+        # print(model.summary())
+        # for name in listdir(directory) :
+        #     filename = directory + '/' + name
+        #     image = load_img(filename, target_size=(224,224))
+        #     image = img_to_array(image)
+        #     image = np.expand_dims(image,axis=0)
+        #     image = preprocess_input(image)
+        #     feature = model.predict(image)
+        #     image_id = name.split('.')[0]
+        #     self.features[image_id] = feature
+        # for i in self.features.keys() :
+        #     self.features[i] = self.features[i].tolist()
+        # with open('features.json', 'w') as f:
+        #     json.dump(self.features,f)  
         with open('features.json', 'r') as f:
             self.features = json.load(f)
         for i in self.features.keys() :
             self.features[i] = np.array(self.features[i])           
-        return self.features    
+        return self.features  
+
+    def load_dataset(self,filename) :
+        f = open(filename,'r')
+        body = f.read()
+        for line in body.split('\n') :
+            image_id = line.split('.')[0]
+            if len(line) < 1:
+                continue
+            self.dataset.append(image_id)
+            self.photo[image_id] = self.features[image_id]
+        f.close()
+        for i in self.df.index:
+            image_id = df['image_id'][i]
+            if image_id in self.dataset:
+                if image_id not in self.description_dataset :
+                    self.description_dataset[image_id] = list()
+                self.description_dataset[image_id].append(df['desc'][i])         
+        return self.photo,self.dataset,self.description_dataset
+
+    def create_sequences(self) :    
+        for key, desc_list in self.description_dataset.items() :
+            for d in desc_list :
+                seq = [self.word_to_ix[word] for word in d.split(' ') if word in self.word_to_ix]
+                for i in range(1,len(seq)) :
+                    in_seq, out_seq = seq[:i], seq[i]
+                    in_seq = pad_sequences([in_seq],maxlen=self.max_len)[0]
+                    out_seq = to_categorical([out_seq],num_classes=len(self.vocab)+1)[0]
+                    self.X1.append(self.photo[key])
+                    self.X2.append(in_seq)
+                    self.y.append(out_seq)
+        self.X1 = np.array(self.X1)
+        self.X2 = np.array(self.X2)
+        self.y = np.array(self.y)
+        return self.X1,self.X2,self.y
+
 if __name__ == "__main__":
     ob  = Dp("D:\Coding_wo_cp\Image_Caption_Generator\Flickr8k.token.txt")
     d = ob.to_dictionary()
@@ -122,3 +170,5 @@ if __name__ == "__main__":
     word_to_ix, ix_to_word = ob.word_index()
     m = ob.maxLength()
     features = ob.feature_extract('D:\Coding_wo_cp\Image_Caption_Generator\Flicker8k_Dataset')   
+    photo,dataset,description_dataset = ob.load_dataset('D:\Coding_wo_cp\Image_Caption_Generator\Flickr_8k.trainImages.txt')
+    X1,X2,y = ob.create_sequences()
